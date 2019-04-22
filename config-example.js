53d21ff70ba69a38f10885729f301760fc726b06
Noa <topoyr@gmail.com>
2019-04-22 21:56:50 +0700

    conf example update

--- config-example.js
+++ config-example.js
@@ -9,5 +9,9 @@ module.exports = {
         chainId: 'b954776664f06dc9986fce64533e28fb9538a8ba008c9e54834ab114c51aadda',
         keyProvider: '5K64tbhRXdt8WqX4eavvtpoTVKSSNXTvDBt4bzH7Mu6c9n4Abof', // faucet active privkey
         httpEndpoint: 'http://127.0.0.1:8888',
-    }
+    },
+    api: {
+        ip: '0.0.0.0',
+        port: 3100,
+    },
 }
