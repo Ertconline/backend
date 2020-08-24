3d689e47a60441f03d957aee95a27a24d3cf6351
Noa <topoyr@gmail.com>
2020-08-24 18:42:06 +0700

    chore: update config example

--- config-example.js
+++ config-example.js
@@ -1,7 +1,11 @@
 module.exports = {
     debug: true,
     db: {
-        url: 'mongodb://localhost:27017',
+        host: '127.0.0.1',
+        port: '27017',
+        username: 'admin',
+        password: 'admin',
+        options: 'test?authSource=ertc&gssapiServiceName=mongodb',
         name: 'ertc',
         collections: {},
     },
